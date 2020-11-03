2b7abb2cb9d5769a13e739b27089563a8f6cc5a4
Nicolas Garcia <nicomat01@gmail.com>
2020-11-02 19:50:08 -0500

    Mapa

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if functionality=='Mapa Demografico':
     print(data.describe())
     st.header("Localizacion Estudiantes por Estrato")
     ESTRATO = st.slider("Estrato", 1, 6)
-    st.map(data[data['Estrato']==ESTRATO][['lat','long']])
+    st.map(data[data['ESTRATO']==ESTRATO][['lat','long']])
 
 if functionality=='Prediccion':
     st.write("""
