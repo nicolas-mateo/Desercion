0330ff95891ae553b043bc2b7ca8cc57c58df334
Nicolas Garcia <nicomat01@gmail.com>
2020-11-02 19:31:42 -0500

    Mapa Corregido

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if functionality=='Mapa Demografico':
         return data    
     data = load_data(100000)
     print(data.describe())
-    st.header("Jopo")
+    st.header("Localizacion Estudiantes por Estrato")
     ESTRATO = st.slider("Estrato", 1, 6)
     st.map(data.query("ESTRATO >= @ESTRATO")[["latitude", "longitude"]].dropna(how="any"))
 
