50e9015431046587743422614b6026f33761f3f8
Milena Martínez P <49167308+milenamartinezp@users.noreply.github.com>
2020-11-07 20:21:06 -0500

    requirements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 DATA_URL = ("caracterizacion_estudiantes_clean.csv")
