62499d2afd5c5a3403306c1eccc3e538eaa33e20
Nicolas Garcia <nicomat01@gmail.com>
2020-11-13 23:36:07 -0500

    plz

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ if functionality=='Información Histórica':
     for r in rangos.ESTADO.unique():
         plot_df = rangos[rangos.ESTADO == r]
         fig9.add_trace(
-            go.Bar(x=[plot_df.PROGRAMA, plot_df.RANGO], y=plot_df.key, name=r,width=1.0),
+            go.Bar(x=[plot_df.PROGRAMA, plot_df.RANGO], y=plot_df.key, name=r),
         )
     for annotation in fig9['layout']['annotations']: 
         annotation['textangle']=-90
