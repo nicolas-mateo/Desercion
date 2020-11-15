7f652a14a7f5cf1dc6ebccf4abd6838cea255fe5
Nicolas Garcia <nicomat01@gmail.com>
2020-11-15 10:34:11 -0500

    Nicolas

--- app.py
+++ app.py
@@ -243,9 +243,9 @@ if functionality=='Informacion Activos':
     proporciones=proporciones.drop(columns=['key_x','key_y'])
     proporciones=proporciones.pivot(index='PROGRAMA',columns='PREDICTION')['Proporcion'].reset_index()
     st.write(proporciones)
-    programas_grad=proporciones.sort_values(by='Proporcion 0',ascending=False).head(5)
-    programas_des=proporciones.sort_values(by='Proporcion 1',ascending=False).head(5)
-    fig24=px.bar(programas_grad,x='PROGRAMA',y=['Proporcion 0','Proporcion 1'],labels={'Proporcion0':'(%)Graduados','Proporcion1':'(%) Desertores'})
+    programas_grad=proporciones.sort_values(by='0',ascending=False).head(5)
+    programas_des=proporciones.sort_values(by='1',ascending=False).head(5)
+    fig24=px.bar(programas_grad,x='PROGRAMA',y=['0','1'],labels={'0':'(%)Graduados','1':'(%) Desertores'})
 
 
     #fig26 = go.Figure([go.Bar(x=programas_des['PROGRAMA'], y=programas_des['key'])])
